ecdc36c2f6699fd0d7ae6427f3677f82bd09ad6a
Phongtlam <Phongtlam@gmail.com>
2017-05-23 12:27:45 -0700

    First version

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,7 +2,7 @@ import webpack from 'webpack';
 import path from 'path';
 
 const config = {
-  entry: './client/src/app',
+  entry: './client/src/app.jsx',
   output: {
     path: path.join(__dirname, 'public/dist'),
     filename: 'bundle.js'
@@ -20,8 +20,41 @@ const config = {
           }
         ]
       }
-    ]
-  }
+    ],
+    loaders: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              query: {
+                name: 'assets/[name].[ext]',
+              },
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              query: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                gifsicle: {
+                  interlaced: true,
+                },
+                optipng: {
+                  optimizationLevel: 7,
+                },
+              },
+            },
+          }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
 };
 
 export default config;
