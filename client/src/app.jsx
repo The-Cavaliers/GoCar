2f165683b078b649637968a46d8b3890d705c1d6
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-05-27 10:56:10 -0700

    Redux Setup

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import View from './components/view.jsx';
 
 class App extends React.Component {
@@ -15,3 +16,31 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+=======
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import store, {history} from './store';
+import Main from './components/main';
+import Index from './components/index';
+import createGroup from './components/createGroup';
+import joinGroup from './components/joinGroup';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+const router =(
+  <MuiThemeProvider>
+    <Provider store={store}> 
+      <Router history={history}>
+        <Route path ='/' component={Index}>
+          <IndexRoute component ={createGroup}></IndexRoute>
+          <Route path ='/createGroup' component={joinGroup}></Route>
+        </Route>
+      </Router>
+    </Provider>
+  </MuiThemeProvider>
+)
+
+ReactDOM.render(router, document.getElementById('root'));
+>>>>>>> Redux Setup
---
+++ client/src/components/createGroup.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class component1 extends React.Component {
+    render() {
+        return(
+          <div>
+            <p>
+              <Link to ="/createGroup"> Component1 </Link> 
+             </p>
+          </div>
+        );
+    }
+} 
+export default component1 ;
