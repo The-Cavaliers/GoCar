d1acaf605b13e271e4b45ca05642323397495f29
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-05-27 14:08:36 -0700

    ReduxSetupV3

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -5,19 +5,19 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {Provider} from 'react-redux';
 import store, {history} from './store';
 import Main from './components/main';
-import Index from './components/index';
+import App from './components/app';
 import createGroup from './components/createGroup';
 import joinGroup from './components/joinGroup';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
 
-injectTapEventPlugin();
+// injectTapEventPlugin();
 
 const router =(
   <MuiThemeProvider>
     <Provider store={store}> 
       <Router history={history}>
-        <Route path ='/' component={Index}>
+        <Route path ='/' component={App}>
           <IndexRoute component ={createGroup}></IndexRoute>
           <Route path ='/createGroup' component={joinGroup}></Route>
         </Route>
